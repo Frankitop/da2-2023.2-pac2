fc988c1426b3de1e7b167b6ffb36921133018fe4
Franco Piriz Menoni <fpiriz88@gmail.com>
2024-06-01 09:40:34 -0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatfactSearchComponent } from './catfact-search/catfact-search.component';
 import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   {path: 'catfacts', component: CatfactSearchComponent},
+  {path: 'detail', component: DetailComponent},
   {path: '', component: HomeComponent}
 ];
 
